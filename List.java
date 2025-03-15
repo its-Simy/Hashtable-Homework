df29e3db66c6f2ca20b5243b90e0561543adfdab
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-03-14 21:27:40 -0400

    Some adjustments
    Adjusted the Node class, along with implementing some of the method functionality from the list class.

--- List.java
+++ List.java
@@ -1,12 +1,15 @@
 public class List {
 //Add the necessary data members
+    private Node head;
+    private int size;
+
 
     /**
      * Default Constructor
      */
     public List(){
-
-
+        head = null;
+        size = 0;
     }
 
     /**
@@ -18,15 +21,22 @@ public class List {
 
     /**
      *A clone class that returns the clone of the current instance
+     * it will call the copy constructor using the current list
+     * then will return the new list that was made.
      */
-    private List createClone(){
-        return new List();
-    }
+    private List createClone(){ return new List(List.this); }
 
     /**
      * This should all a player to the linked list
      */
     public void add(Player p){
+        if(head == null)
+            head = new Node(p);
+        else{
+            Node temp = head;
+            head = new Node(p);
+            head.next = temp;
+        }
 
     }
 
@@ -34,10 +44,15 @@ public class List {
      *This will return the player with the same name as the one in the signature
      */
     public Player find(String name){
-        return null;
+        Node iteration = head;
+        for(int i = 0; i < size; i++){
+            if(iteration.getPlayer().getName().equals(name))
+                break;
+
+            iteration = iteration.next;
+        }
+        return iteration.getPlayer();
     }
-
-
 
 
 
--- Node.java
+++ Node.java
@@ -1,8 +1,20 @@
+/**
+ * This class will create the node objects of type player for the linked list
+ */
 public class Node {
+    Player player;
     Node next;// pointer to the next element
-    int data;
-    Node(int _data){
-        this.next = null;
-        this.data = _data;
+
+    /**
+     * Instantiates the node with the player data
+     */
+    public Node(Player p) { this.player = p;}
+
+    /**
+     * This is a getter that returns the player information
+     */
+    public Player getPlayer() {
+        return player;
     }
+
 }
