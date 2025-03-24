2ce07a61d87debe14dd06c842073b70c920e367a
its-Simy <135281067+its-Simy@users.noreply.github.com>
2025-03-24 08:46:39 -0400

    Finished program demonstration
    Complete demonstration of the copy constructor, find method, and add/resizing from the add method and createClone method.

--- Main.java
+++ Main.java
@@ -1,41 +1,17 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
         HashTable hashTable = new HashTable();
-        /*
-        Player player1 = new Player();
-        Player player2 = new Player("Juan", 1000);
-        Player player3 = new Player("Oscar", 50);
-        Player player4 = new Player("maaz", 60);
-        Player player5 = new Player("arthur", 70);
-        Player player6 = new Player("alshibli", 10);
-        Player player7 = new Player("aaron", 90);
-        Player player8 = new Player("Jose", 20);
-        Player player9 = new Player("logan", 30);
-        hashTable.add(player1);
-        hashTable.add(player2);
-        hashTable.add(player3);
-        hashTable.add(player4);
-        hashTable.add(player5);
-        hashTable.add(player6);
-        hashTable.add(player7);
-        hashTable.add(player8);
-        hashTable.add(player9);
-
-         */
-
-
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("info.txt"));
             String line = reader.readLine();
             String name = "";
             int score = 0;
-            while(!(line.equals(""))){
+            while(line != null) {
                 //if the line is a Name, then it should, cast that as the name
                     name = line;
                     line = reader.readLine();
@@ -49,29 +25,52 @@ public class Main {
             e.printStackTrace();
         }
 
-
         System.out.println("\n The following are the players inside of the hashtable \n");
-        //prints out all the players in the hashtable
         hashTable.show();
 
 
-
-        /*
         HashTable hashTable2 = new HashTable(hashTable);
-        System.out.println("\n The following are the players inside of the copied hashtable using copy constructor\n");
-        //hashTable2.show();
+        System.out.println("\nThe following are the players inside of the copied hashtable using copy constructor:\n");
+        hashTable2.show();
 
+        System.out.println("\nThis is the deep copy of the hashtable before modification:\n");
         HashTable hashTable3 = new HashTable(hashTable2.createClone());
-        //Player player2 = new Player("John Smith", 1);
-        //hashTable3.add(player2);
-        //hashTable3.show();
-        //System.out.println("This was the one that was copied\n");
-        //hashTable2.show();
- */
-        System.out.println();
+        hashTable3.show();
 
-        System.out.println(hashTable.find("Matthew Sanders").getScore());
-        
+
+        Player player2 = new Player("John Smith", 1);
+        hashTable3.add(player2);
+        System.out.println("\nThis is the deep copy of the hashtable after modification:\n");
+        hashTable3.show();
+
+        System.out.println("\nThis is the original hashtable that was copied:\n");
+        hashTable2.show();
+
+
+        System.out.println("\nThis is the demonstration of the clone method:\n");
+
+        System.out.println("\nThis is the original hashtable before modification:\n");
+        hashTable2.show();
+
+        HashTable hashTable4 = new HashTable();
+        hashTable4 = hashTable2.createClone();
+
+        System.out.println("\nThis is the deep copy using the clone method:\n");
+        hashTable4.show();
+
+
+        System.out.println("\nThis is the original hashtable after modification:\n");
+        hashTable2.add(player2);
+        hashTable2.show();
+
+        System.out.println("\nThis is the clone hashtable after original got modified:\n");
+        hashTable4.show();
+
+
+        String playerName = "Matthew Sanders";
+        System.out.println("\nThe following demostrates the Find method for the player " + playerName + ":\n");
+        System.out.println(playerName + "'s score: " + hashTable.find(playerName).getScore());
+
 
 
     }//end of the main method
